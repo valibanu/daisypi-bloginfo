4e568d6753a2c1685c710100e4a8698634da05e7
Valerian Banu <vali.banu@gmail.com>
2013-12-20 02:16:42 +0200

    removed BeautifulSoup dependency

--- pagerank/twitter.py
+++ pagerank/twitter.py
@@ -2,7 +2,6 @@ import httplib
 import re
 import urllib2
 
-from BeautifulSoup import BeautifulSoup
 
 __author__ = 'valibanu'
 
@@ -16,7 +15,6 @@ def get_twitter_info(twitter_id):
     response = urllib2.build_opener().open(query, timeout=10)
     if response.getcode() == httplib.OK:
         data = response.read()
-        soup = BeautifulSoup(data)
-        print('Twitter followers: {}'.format(re.search(r'"followers_count":('
-                                                       r'\d+)',
-                                                       str(soup)).group(1)))
+        print('Twitter followers: {}'.format(re.search(r'&quot;followers_count'
+                                                       r'&quot;:('r'\d+)',
+                                                       str(data)).group(1)))
